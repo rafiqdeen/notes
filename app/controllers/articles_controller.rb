fe0d9e0b2040a545601bf69a35565af1a4608feb
Rafiq <rafiq@thydreamtech.com>
2015-02-02 16:14:32 +0530

    show and destroy added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,11 +11,29 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def edit
+
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    @article.update(article_params)
+    redirect_to @article
+  end
+
   def create
     @article = Article.new(article_params)
 
     @article.save
-    redirect_to @article
+    redirect_to root_path
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+
+    @article.destroy
+    redirect_to root_path
   end
 
   private
